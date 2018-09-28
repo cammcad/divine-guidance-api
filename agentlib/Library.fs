e3766c7a4fa9acd823370fbe97363e26c46bfd80
fredec4 <cameron.frederick@virtustream.com>
2018-09-28 14:16:27 -0400

    re-organization and adding inital agent library

---
+++ agentlib/Library.fs
@@ -0,0 +1,5 @@
+namespace agentlib
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ api/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Suave
+open Suave.Operators
+
+[<EntryPoint>]
+let main _argv =
+    let app =
+        choose
+            [
+                Suave.Filters.path "/v1/healthcheck" >=> Suave.Filters.GET >=> Successful.OK ""
+                RequestErrors.NOT_FOUND "Not Found"
+            ] 
+
+    startWebServer { defaultConfig with hideHeader=true; bindings = [HttpBinding.create HTTP (System.Net.IPAddress.Parse("0.0.0.0")) (Sockets.Port.Parse "3456")] } app
+    0 // return an integer exit code
